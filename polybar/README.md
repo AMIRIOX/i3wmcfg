4199a729e67866afb286208580794fd638b87426
AMIRIOX <wumingyun2120@outlook.com>
2024-04-08 05:19:10 +0800

    [5] polybar totally changed

---
+++ polybar/README.md
@@ -0,0 +1,2 @@
+These files are modified from `These files are modified from these files and follow the MIT protocol`, 
+and follow the MIT protocol
--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,10 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-killall -q polybar
+# Terminate already running bar instances
+killall polybar
+# pkill -f polybar
 
+# Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar example &
-
-echo "Polybar launched..."
-
+# Launch polybar
+# polybar top -c $HOME/.config/polybar/nord/config-top.ini &
+# polybar bottom -c $HOME/.config/polybar/nord/config-bottom.ini &
+#
+if type "xrandr"; then
+  for m in $(polybar --list-monitors | cut -d":" -f1); do
+      # MONITOR=$m polybar --reload example &
+      MONITOR=$m polybar top -c $HOME/.config/polybar/nord/config-top.ini &
+  done
+  # for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+  #   # MONITOR=$m polybar --reload example &
+  #   MONITOR=$m polybar top -c $HOME/.config/polybar/nord/config-top.ini &
+  # done
+else
+  polybar top -c $HOME/.config/polybar/nord/config-top.ini &
+fi
